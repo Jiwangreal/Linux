31d177b28dffdd85d7f6591146d5cdb046b9b380
JiWangreal <48763621+Jiwangreal@users.noreply.github.com>
2021-02-08 09:57:04 +0800

    modify P6addr.c

--- LinuxNetworkProgramming/P6addr.c
+++ LinuxNetworkProgramming/P6addr.c
@@ -5,7 +5,7 @@ int main()
 {
     unsigned long addr = inet_addr("192.168.0.100");
 
-    printf("addr = %u \n", ntoh(addr));//u：无符号方式输出
+    printf("addr = %u \n", ntohl(addr));//u：无符号方式输出
 
     struct in_addr ipaddr;
     ipaddr.s_addr=addr;
